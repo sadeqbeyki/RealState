2dca7c4e639e87f1eb2eae41497a09617650f1c1
mardibak <mardibak@outlook.com>
2022-10-28 12:17:14 +0330

    product entity implemented

---
+++ RS.Domain/Entities/Product/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RS.Domain.Entities.Product
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int size { get; set; }
+        public int price { get; set; }
+    }
+}
