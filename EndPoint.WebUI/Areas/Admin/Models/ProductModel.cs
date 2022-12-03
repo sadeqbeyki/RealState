58137bfbe47af055d0a24046765c79f316474f48
mardibak <mardibaki@gmail.com>
2022-12-03 22:18:36 +0330

    product dto added

--- EndPoint.WebUI/Areas/Admin/Models/ProductModel.cs
+++ EndPoint.WebUI/Areas/Admin/Models/ProductModel.cs
@@ -1,8 +1,9 @@
 ﻿namespace RS.Contracts.Products;
 
-public class ProductModel
+public class ProductDto
 {
     public string Name { get; set; }
     public string Code { get; set; }
     public long CategoryId { get; set; }
+    public List<ProductViewModel> Products { get; set; }
 }
