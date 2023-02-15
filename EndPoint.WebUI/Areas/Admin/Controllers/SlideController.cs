9b0822b5064622816286df2a5dffb838b993ec90
mardibak <mardibaki@gmail.com>
2023-02-15 23:22:45 +0330

    add login page and ui view for this

--- EndPoint.WebUI/Areas/Admin/Controllers/SlideController.cs
+++ EndPoint.WebUI/Areas/Admin/Controllers/SlideController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using RS.Contracts.Slides;
 
 namespace EndPoint.WebUI.Areas.Admin.Controllers;
 [Area("Admin")]
+[Authorize]
 public class SlideController : Controller
 {
     [TempData]
