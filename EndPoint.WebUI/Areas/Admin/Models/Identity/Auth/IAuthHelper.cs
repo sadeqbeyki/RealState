8d896160c75ea53396f17b18e2ae227362924f51
mardibak <mardibaki@gmail.com>
2023-02-19 20:11:03 +0330

    auth helper implemented

---
+++ EndPoint.WebUI/Areas/Admin/Models/Identity/Auth/IAuthHelper.cs
@@ -0,0 +1,16 @@
+﻿using AppFramework;
+
+namespace EndPoint.WebUI.Areas.Admin.Models.Identity.Auth
+{
+    public interface IAuthHelper
+    {
+        void SignOut();
+        bool IsAuthenticated();
+        void SignIn(AuthViewModel account);
+        string CurrentAccountRole();
+        AuthViewModel CurrentAccountInfo();
+        List<int> GetPermissions();
+        long CurrentAccountId();
+        string CurrentAccountMobile();
+    }
+}
