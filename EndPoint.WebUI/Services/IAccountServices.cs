beccf3aed0394b4f717f3f40e9ccc3ee5bfb83ac
mardibak <mardibaki@gmail.com>
2023-03-05 23:12:06 +0330

    iaccount services updated

--- EndPoint.WebUI/Services/IAccountServices.cs
+++ EndPoint.WebUI/Services/IAccountServices.cs
@@ -1,6 +1,23 @@
-﻿namespace EndPoint.WebUI.Services
+﻿using EndPoint.WebUI.Areas.Admin.Models;
+using EndPoint.WebUI.Areas.Admin.Models.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EndPoint.WebUI.Services;
+
+public interface IAccountServices
 {
-    public class IAccountServices
+    Task<AppLoginViewModel> Login(AppLoginViewModel model);
+    CreateUserViewModel Register(CreateUserViewModel model);
+}
+public class AccountServices : IAccountServices
+{
+    public Task<AppLoginViewModel> Login(AppLoginViewModel model)
     {
+        throw new NotImplementedException();
+    }
+
+    public CreateUserViewModel Register(CreateUserViewModel model)
+    {
+        throw new NotImplementedException();
     }
 }
